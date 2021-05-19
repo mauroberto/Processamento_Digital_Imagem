f4e441da15af6a5726768c04acf1113d2dd622dd
mauroberto <maurorcsc@gmail.com>
2021-05-19 20:22:31 -0300

    algumas correções

--- trabalho3/trabalho3.py
+++ trabalho3/trabalho3.py
@@ -5,10 +5,11 @@ import argparse
 import random as rand
 import matplotlib.pyplot as plt
 
+#seed para a gerar cores aleatórias
 rand.seed(192800)
 
 def histogram(areas, output_filename):
-    n, bins, patches = plt.hist(areas, bins=[min(areas), 1500, 3000, max(areas)], edgecolor='black', linewidth=1)
+    n, bins, patches = plt.hist(areas, bins=[min(areas), 1500, 3000, max(max(areas), 4000)], edgecolor='black', linewidth=1)
     plt.xlabel("Área")
     plt.ylabel("Número de Objetos")
     plt.savefig(output_filename[:-4]+"_histograma"+output_filename[-4:])
@@ -20,37 +21,45 @@ def histogram(areas, output_filename):
 
 def getCentroid(cnt):
     M = cv2.moments(cnt)
-    cx = int(M['m10']/M['m00'])
-    cy = int(M['m01']/M['m00'])
+    if M['m00'] != 0:
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+    else:
+        cx, cy = cnt[0][0]
 
     return (cx, cy)
 
 def getSolidity(cnt, area):
-    #convex hull
+    #fecho convexo
     convex_hull = cv2.convexHull(cnt)
 
-    #convex hull area
+    #area do fecho convexo
     convex_area = cv2.contourArea(convex_hull)
 
-    #solidity = contour area / convex hull area
+    if convex_area == 0:
+        return 0.0
+
+    #solidez = area / area do fecho convexo
     return area/float(convex_area)
 
 def getEccentricity(cnt):
+    if cnt.shape[0] < 5:
+        return 0.0 
+
     ellipse = cv2.fitEllipse(cnt)
 
-    #center, axis_length and orientation of ellipse
-    (center, axes, orientation) = ellipse
+    #centro, tamanho dos eixos e orientação da elipse
+    (center, axis, orientation) = ellipse
 
-    #length of major and minor axis
-    majoraxis_length = max(axes)
-    minoraxis_length = min(axes)
+    #comprimento do maior e menor eixo
+    max_axle = max(axis)
+    min_axle = min(axis)
 
-    #eccentricity
-    return np.sqrt(1-(minoraxis_length/majoraxis_length)**2)
+    return np.sqrt(1-(min_axle/max_axle)**2)
 
 def drawContours(contours, edged, hierarchy):
     drawing = np.full((edged.shape[0], edged.shape[1], 3), 255, dtype=np.uint8)
-    for i in range(len(contours)):
+    for i in range(0, len(contours)):
         color = (0, 0, 255)
         cv2.drawContours(drawing, contours, i, color, 1, cv2.LINE_8, hierarchy, 0)
 
@@ -58,33 +67,46 @@ def drawContours(contours, edged, hierarchy):
 
 def drawContoursFilled(contours, edged):
     drawing = np.full((edged.shape[0], edged.shape[1], 3), 255, dtype=np.uint8)
-    for i in range(len(contours)):
+    for i in range(0, len(contours)):
         cnt = contours[i]
         color = (rand.randint(0, 255), rand.randint(0, 255), rand.randint(0, 255))
         cv2.fillPoly(drawing, pts = [cnt], color=color)
-        cv2.putText(drawing, str(i), getCentroid(cnt), cv2.FONT_HERSHEY_PLAIN, 1, 0)
+        (cX, cY) = getCentroid(cnt)
+        cv2.putText(drawing, str(i), (cX, cY), cv2.FONT_HERSHEY_PLAIN, 1, 2)
 
     return drawing
 
 def extractProperties(contours):
     areas = np.zeros((len(contours)))
 
-    print("número de regiões: %d" % len(contours))
+    print("número de regiões: %d" % len(contours), end="\n\n")
 
     for i in range(0, len(contours)):
         cnt = contours[i]
         area = cv2.contourArea(cnt)
         areas[i] = area
         print("região %d:" % (i), end=" ")
-        print("área: %f" % area, end=" ")
+        print("área: %.0f" % area, end=" ")
         print("perímetro: %f" % cv2.arcLength(cnt,True), end=" ")
         print("excentricidade: %f" % getEccentricity(cnt), end=" ")
         print("solidez: %f" % getSolidity(cnt, area))
 
+    print("")
+
     return areas
 
+def filterContoursByHierarchy(contours, hierarchy):
+    cntours = []
+
+    for i in range(0, len(contours)):
+        if hierarchy[0][i][3] == -1:
+            continue
+        cntours.append(contours[i])
+
+    return cntours
+
 def main():
-    #read args
+    #lendo argumentos
     parser = argparse.ArgumentParser()
     parser.add_argument('--img', '-i', help="Image Path", type=str)
     parser.add_argument('--out', '-o', help="Output filename is optional. If not passed, the default value is './output.png'", type=str, default='./output.png')
@@ -102,29 +124,31 @@ def main():
         print(parser.format_help())
         exit()
 
-    #read image as grayscale
+    #lendo imagem em tons de cinza
     img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
 
-    #converting image to black and white
-    (thresh, im_bw) = cv2.threshold(img, 230, 255, cv2.THRESH_BINARY)
+    #convertendo imagem para preto e branco
+    (thresh, im_bw) = cv2.threshold(img, 250, 255, cv2.THRESH_BINARY)
     cv2.imwrite(output_filename[:-4]+"_binaria"+output_filename[-4:], im_bw)
 
-    edged = cv2.Canny(im_bw, 30, 200)
+    edged = cv2.Canny(im_bw, 100, 200)
     _, contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.imwrite(output_filename[:-4]+"_thresh"+output_filename[-4:], edged)
+
+    #filtrando os contornos por hierarquia (cv2.RETR_TREE)
+    #contours = filterContoursByHierarchy(contours, hierarchy)
     
-    #1.2 - draw contours
+    #1.2 - desenhando contornos
     drawing = drawContours(contours, edged, hierarchy)
     cv2.imwrite(output_filename[:-4]+"_contornos"+output_filename[-4:], drawing)
 
-    #1.3 - extracting and printing properties from contours
+    #1.3 - extraindo e imprimindo as propriedades dos contornos
     areas = extractProperties(contours)
 
-    #1.3 - draw contours, fill and label them
+    #1.3 - desenhando contornos preenchidos e com rótulos
     drawing2 = drawContoursFilled(contours, edged)
     cv2.imwrite(output_filename[:-4]+"_contornos_preenchidos"+output_filename[-4:], drawing2)
 
-    #1.4 - histogram
+    #1.4 - histograma de área dos objetos
     histogram(areas, output_filename)
 
 
